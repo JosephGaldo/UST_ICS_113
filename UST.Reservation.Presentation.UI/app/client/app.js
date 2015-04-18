541f2e6962b2ab0b2641cca1d5bd60a6cb32e724
Brian Kevin <bkferareza@gmail.com>
2015-04-19 00:57:20 +0800

    Fixed sign-up

---
+++ UST.Reservation.Presentation.UI/app/client/app.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function () {
+    $("#Sign_up_Employee").submit(function (event) {
+        event.preventDefault();
+        var pass1 = $( 'input[name=password1]' ).val();
+		var pass2 = $( 'input[name=password2]' ).val();
+        if (pass1 != pass2) 
+		{
+			toastr.error('Passwords should be match!', 'Password Error');
+			return true;
+		}
+		else
+		{
+			toastr.success('Registration Success!');
+			$('#Sign_up_Employee').trigger("reset");
+			$('#employeesignupModal').modal('hide');
+			
+		}
+    });
+});
+
+$(document).ready(function () {
+    $("#Sign_up_student").submit(function (event) {
+        event.preventDefault();
+        var pass1 = $( 'input[name=password1student]' ).val();
+		var pass2 = $( 'input[name=password2student]' ).val();
+        if (pass1 != pass2) 
+		{
+			toastr.error('Passwords should be match!', 'Password Error');
+			return true;
+		}
+		else
+		{
+			toastr.success('Registration Success!');
+			$('#Sign_up_student').trigger("reset");
+			$('#studentsignupModal').modal('hide');
+			
+		}
+    });
+});
+
+
