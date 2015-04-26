a5aef663accbb26702ed576b6732f49d00728d2d
Brian Kevin <bkferareza@gmail.com>
2015-04-26 22:46:46 +0800

    For Checkpoint

---
+++ UST.Reservation.Presentation.UI/src/Login.java
@@ -0,0 +1,36 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String idno=request.getParameter("idno");
+		String pword=request.getParameter("pw");
+		
+		RequestDispatcher rd=request.getRequestDispatcher("/reservation.jsp");
+		request.setAttribute(idno, pword);
+		rd.forward(request, response);
+		
+		RequestDispatcher rd2=request.getRequestDispatcher("/thankyou.jsp");
+		request.setAttribute(idno, pword);
+		rd.forward(request, response);
+	}
+
+}
